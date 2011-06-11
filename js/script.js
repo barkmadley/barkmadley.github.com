34839264aa8a789fabc8427710fc2fe5d639df96
Mark Bradley <barkmadley@gmail.com>
2011-06-11 20:23:33 +1000

    add random posts based on a knockout template.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
-/* Author: 
-
+/* Author: Mark Bradley
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* if a page has an init function defined, run that function once the page has
+ * finished loading */
+$(function () {
+  if (page_init) {
+    page_init();
+  }
+});
 
